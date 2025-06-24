fe436155dfff172a4a8fd41abbc76c81508a50b4
alpharabbit9 <alpharabbit74@gmail.com>
2025-06-25 01:52:13 +0600

    completed faculty page just details left

--- src/Faculty/Department/THM/THM.jsx
+++ src/Faculty/Department/THM/THM.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import FacultyCard from '../../FacultyCard/facultyCard'
 
 const THM = () => {
+    const [teachers, setTeacher] = useState([]);
+
+    useEffect(() => {
+        fetch('/faculty_thm.json')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setTeacher(data)
+
+            })
+    }, [])
     return (
         <div>
-            thm
+
+            <p className="font-bold text-gray-700 mb-6">Faculty({teachers.length})</p>
+
+            <div className="flex flex-wrap gap-3">
+                {
+                    teachers.map(teacher => <FacultyCard teacher={teacher}></FacultyCard>)
+                }
+            </div>
+
         </div>
     );
 };
